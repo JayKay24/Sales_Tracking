2745aac411f532deccd3f10c8fbc5dc137b92ac8
James Kinyua <jameskinyua590@gmail.com>
2024-03-21 12:47:58 +0300

    create schema for user

---
+++ packages/server/src/user/user.schema.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+type UserDocument = HydratedDocument<User>;
+
+@Schema()
+class User {
+  @Prop({ required: true })
+  first_name: string;
+
+  @Prop({ required: true })
+  last_name: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  role: string;
+
+  @Prop({ required: true })
+  phone_number: string;
+
+  @Prop({ default: '' })
+  county: string;
+}
+
+const UserSchema = SchemaFactory.createForClass(User);
+
+export { UserSchema, UserDocument };
