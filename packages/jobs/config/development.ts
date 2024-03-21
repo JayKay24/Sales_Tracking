28341963b656575bd03606be4c24d916c5109689
James Kinyua <jameskinyua590@gmail.com>
2024-03-21 17:56:12 +0300

    remove jwt secret from jobs

--- packages/jobs/config/development.ts
+++ packages/jobs/config/development.ts
@@ -2,9 +2,8 @@ import { IConfig } from './iConfig';
 
 const devConfig: IConfig = {
   ENV: 'development',
-  db_conn_string: process.env.CONN_STR_LOCAL,
-  jwt_secret: process.env.JWT_SECRET,
-  db_password: process.env.DB_PASSWORD,
+  db_conn_string: process.env.JOBS_CONN_STR_LOCAL,
+  db_password: process.env.JOBS_DB_PASSWORD,
 };
 
 export default devConfig;
