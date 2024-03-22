2c780ffef0cdc7d8f4eea5197600101e3b093696
James Kinyua <jameskinyua590@gmail.com>
2024-03-22 23:01:15 +0300

    implement add user service method

--- packages/server/src/user/user.service.ts
+++ packages/server/src/user/user.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { User, UserRole } from './user.schema';
+
+import * as bcrypt from 'bcrypt';
+import { saltOrRounds } from 'auth/auth.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userModel: Model<User>) {}
+
+  async addUser(
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    role: UserRole,
+    phoneNumber: string,
+    county: string = '',
+  ) {
+    const existingUser = await this.userModel.findOne({ email });
+    if (existingUser) {
+      throw new ConflictException('User with that email already exists');
+    }
+    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+    const newUser = new this.userModel({
+      first_name: firstName,
+      last_name: lastName,
+      role: role,
+      phone_number: phoneNumber,
+      password: hashedPassword,
+      county,
+    });
+    const user = await newUser.save();
+    return {
+      id: user._id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      role: user.role,
+      email: user.email,
+      county: user.county,
+    };
+  }
+
+  async updateUser(
+    id: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    role: UserRole,
+    phone_number: string,
+    county: string = '',
+  ) {}
+}
