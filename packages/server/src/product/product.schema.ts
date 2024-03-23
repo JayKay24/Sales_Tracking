7b6056b93cddc7224e03c9f22a17fddcfe1166ff
James Kinyua <jameskinyua590@gmail.com>
2024-03-23 15:44:15 +0300

    create product schema for mongoose

---
+++ packages/server/src/product/product.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { User } from 'user/user.schema';
+
+enum ProductCategory {
+  MEDICAL = 'medical',
+  FOOD_BEVERAGES = 'food_beverages',
+  CONSTRUCTION = 'construction',
+  OFFICE_SUPPLIES = 'office_supplies',
+  TECHNOLOGY = 'technology',
+}
+
+type ProductDocument = HydratedDocument<Product>;
+
+@Schema({ timestamps: true })
+class Product {
+  @Prop({ required: true, unique: true })
+  name: string;
+
+  @Prop({ required: true })
+  category: ProductCategory;
+
+  @Prop({ required: true })
+  price: number;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  agent_id: User;
+}
+
+const ProductSchema = SchemaFactory.createForClass(Product);
+
+export { ProductSchema, ProductDocument, ProductCategory };
