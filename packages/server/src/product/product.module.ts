82909c1b18c8f5c420303235781b69db2bfd5adb
James Kinyua <jameskinyua590@gmail.com>
2024-03-23 23:02:32 +0300

    remove unused product schema

--- packages/server/src/product/product.module.ts
+++ packages/server/src/product/product.module.ts
@@ -4,15 +4,11 @@ import { ProductSchema } from './product.schema';
 import { ProductController } from './product.controller';
 import { UserService } from 'user/user.service';
 import { JwtService } from '@nestjs/jwt';
-import { UserSchema } from 'user/user.schema';
 import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: 'Product', schema: ProductSchema },
-      { name: 'User', schema: UserSchema },
-    ]),
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
   ],
   controllers: [ProductController],
   providers: [UserService, JwtService, ConfigService],
