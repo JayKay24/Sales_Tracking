02d8b206d25046fc9585409700654818d9635538
James Kinyua <jameskinyua590@gmail.com>
2024-03-23 18:14:54 +0300

    implement addProduct route

--- packages/server/src/product/product.controller.ts
+++ packages/server/src/product/product.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Headers, Post, UseGuards } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ProductService } from './product.service';
+import { JwtAuthGuard } from 'auth/jwt.guard';
+import { ProductDtoCreate } from './dto/product.dto';
+import { ConfigService } from '@nestjs/config';
 
-@Controller('product')
-export class ProductController {}
+@Controller('api/v1/products')
+export class ProductController {
+  constructor(
+    private jwtService: JwtService,
+    private productService: ProductService,
+    private configService: ConfigService,
+  ) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async addProduct(
+    @Body() product: ProductDtoCreate,
+    @Headers('Authorization') token: string,
+  ) {
+    const payload = this.extractPayload(token);
+    const newProduct = await this.productService.addProduct(
+      payload.email,
+      product.name,
+      product.category,
+      product.price,
+    );
+
+    return newProduct;
+  }
+
+  private extractPayload(token: string) {
+    const [, originalToken] = token.split(' ');
+    const payload = this.jwtService.verify(originalToken, {
+      secret: this.configService.get<string>('JWT_SECRET'),
+    });
+
+    return payload;
+  }
+}
--- packages/server/src/product/product.module.ts
+++ packages/server/src/product/product.module.ts
@@ -4,12 +4,17 @@ import { ProductSchema } from './product.schema';
 import { ProductController } from './product.controller';
 import { UserService } from 'user/user.service';
 import { JwtService } from '@nestjs/jwt';
+import { UserSchema } from 'user/user.schema';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+    MongooseModule.forFeature([
+      { name: 'Product', schema: ProductSchema },
+      { name: 'User', schema: UserSchema },
+    ]),
   ],
   controllers: [ProductController],
-  providers: [UserService, JwtService],
+  providers: [UserService, JwtService, ConfigService],
 })
 export class ProductModule {}
