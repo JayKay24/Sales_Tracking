575af0c87ca7452a9177586a4ec8e1f42e41e13f
James Kinyua <jameskinyua590@gmail.com>
2024-03-24 22:08:10 +0300

    document auth controller

--- packages/server/src/auth/auth.controller.ts
+++ packages/server/src/auth/auth.controller.ts
@@ -1,11 +1,24 @@
 import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LoginDto } from './dto/auth.dto';
+import { LoginDto, LoginResponse } from './dto/auth.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('api/v1/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiResponse({
+    description: 'Invalid login credentials',
+    status: 401,
+  })
+  @ApiResponse({
+    description: 'access token for future requests',
+    type: LoginResponse,
+    status: 200,
+  })
+  @ApiOperation({
+    description: 'login to get access token for future requests',
+  })
   @Post()
   async login(@Body() creds: LoginDto) {
     try {
--- packages/server/src/auth/dto/auth.dto.ts
+++ packages/server/src/auth/dto/auth.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsNotEmpty,
@@ -7,12 +8,26 @@ import {
 } from 'class-validator';
 
 class LoginDto {
+  @ApiProperty({
+    description: 'email of registered user',
+    example: 'bruce@example.com',
+    type: 'string',
+    minLength: 10,
+    maxLength: 30,
+  })
   @IsEmail()
   @IsNotEmpty()
   @MinLength(10)
   @MaxLength(30)
   email: string;
 
+  @ApiProperty({
+    description: 'password of registered user',
+    example: 'bruce123',
+    type: 'string',
+    minLength: 5,
+    maxLength: 40,
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(5)
@@ -20,4 +35,9 @@ class LoginDto {
   password: string;
 }
 
-export { LoginDto };
+class LoginResponse {
+  @ApiProperty()
+  access_token: string;
+}
+
+export { LoginDto, LoginResponse };
