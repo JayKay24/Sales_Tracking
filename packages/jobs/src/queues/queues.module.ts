37d070d1dacddbe6df00b47c10c5b82460ee4243
James Kinyua <jameskinyua590@gmail.com>
2024-03-24 12:39:36 +0300

    configure sales schema

--- packages/jobs/src/queues/queues.module.ts
+++ packages/jobs/src/queues/queues.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ConsumerQueuesService } from './queues.service';
 import { SalesService } from 'sales/sales.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SaleSchema } from 'sales/sales.schema';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forFeature([{ name: 'Sale', schema: SaleSchema }]),
+  ],
   providers: [ConsumerQueuesService, SalesService, ConfigService],
 })
 export class QueuesModule {}
