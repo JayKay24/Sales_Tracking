70d851345431bc1123415b9dec515e481d86ee79
James Kinyua <jameskinyua590@gmail.com>
2024-03-25 18:46:04 +0300

    provide EmailService

--- packages/jobs/src/sales/sales.module.ts
+++ packages/jobs/src/sales/sales.module.ts
@@ -4,9 +4,10 @@ import { SaleSchema } from './sales.schema';
 import { SalesService } from './sales.service';
 import { CommissionService } from 'commission/commission.service';
 import { CommissionSchema } from 'commission/commission.schema';
+import { EmailService } from 'email/email.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Sale', schema: SaleSchema }, { name: 'Commission', schema: CommissionSchema }])],
-  providers: [SalesService, CommissionService],
+  providers: [SalesService, CommissionService, EmailService],
 })
 export class SalesModule {}
