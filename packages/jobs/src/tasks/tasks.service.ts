382fcb88ae8abfb98175e780da8219014eb9f0f6
James Kinyua <jameskinyua590@gmail.com>
2024-03-26 16:18:00 +0300

    log when cron job runs

--- packages/jobs/src/tasks/tasks.service.ts
+++ packages/jobs/src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Cron } from '@nestjs/schedule';
 import { CommissionService } from 'commission/commission.service';
@@ -8,6 +8,7 @@ import { Sale } from 'sales/sales.schema';
 
 @Injectable()
 export class TasksService {
+  private logger = new Logger(TasksService.name);
   constructor(
     @InjectModel('Sale') private salesModel: Model<Sale>,
     private emailService: EmailService,
@@ -16,6 +17,7 @@ export class TasksService {
 
   @Cron('0 0 0 */15 * *') // run evey 15 days
   async handleCron() {
+    this.logger.log('Run cron job to send emails to agents every 15 days');
     const agentIds = await this.salesModel.distinct('agent_id').exec();
     const today = new Date();
     const fifteenDaysAgo = new Date(today.getTime() - 15 * 24 * 60 * 60);
@@ -23,11 +25,11 @@ export class TasksService {
     agentIds.forEach(async (id) => {
       const unpaidCommissions =
         await this.commissionService.getUnPaidCommissions(id, fifteenDaysAgo);
-      const agent = await this.salesModel.findById(id);
+      const agent = await this.salesModel.findOne({ agent_id: id }).exec();
       await this.emailService.sendEmail(
         agent.agent_email,
         'Your total unpaid commissions so far',
-        `Here's your total unpaid commissions since 15 days ago, Total unpaid${unpaidCommissions}`,
+        `Here's your total unpaid commissions since 15 days ago, \nTotal unpaid: ${unpaidCommissions}`,
       );
     });
   }
